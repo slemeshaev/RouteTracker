69a5e3610fc138b1e45d23e9a57551bfa8a12888
Stanislav <lemeshaevs@gmail.com>
2020-09-23 19:09:13 +0300

    Install GoogleMaps

--- RouteTracker/RouteTracker/AppDelegate.swift
+++ RouteTracker/RouteTracker/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleMaps // Импорт фреймворка
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Настройка ключа
+        GMSServices.provideAPIKey("AIzaSyCs0DeDIhINA4XRWvLmWHpK-TFadgYbmVA")
         return true
     }
 
